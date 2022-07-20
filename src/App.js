87fc4aa1185ce3ffc878412088284db5359dc2a3
spacescript <simonbengoehring@gmail.com>
2022-07-19 20:01:38 -0700

    MkDown Previewer Pack 2

--- src/App.js
+++ src/App.js
@@ -3,19 +3,12 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container" id="container">
+        <div className="editor-box" id="editor-box">
+          <textarea name="editor" id="editor" cols="30" rows="10"></textarea>
+        </div>
+        <div className="preview" id="preview"></div>
+      </div>
     </div>
   );
 }
