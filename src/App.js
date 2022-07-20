38792715d601d4f66f56737a9f96800a1466d145
spacescript <simonbengoehring@gmail.com>
2022-07-20 12:37:00 -0700

    Mkdown Previewer Pack 3
    Set basic styling for seeing divs

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ function App() {
     <div className="App">
       <div className="container" id="container">
         <div className="editor-box" id="editor-box">
+          <h3>Editor</h3>
           <textarea name="editor" id="editor" cols="30" rows="10"></textarea>
         </div>
-        <div className="preview" id="preview"></div>
+        <div className="preview-box" id="preview-box">
+          <h3>Preview</h3>
+          <div className="preview" id="preview"></div>
+        </div>
       </div>
     </div>
   );
