9b990f752809684b9ba83ecef14a8d201174cc50
spacescript <simonbengoehring@gmail.com>
2022-07-20 21:34:00 -0700

    MkDown Previewer Pack 4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.scss';
 
 function App() {
+
+
+
   return (
     <div className="App">
       <div className="container" id="container">
         <div className="editor-box" id="editor-box">
           <h3>Editor</h3>
-          <textarea name="editor" id="editor" cols="30" rows="10"></textarea>
+          <textarea name="editor" id="editor" cols="30" rows="10" placeholder="Content"></textarea>
         </div>
         <div className="preview-box" id="preview-box">
           <h3>Preview</h3>
-          <div className="preview" id="preview"></div>
+          <div className="preview" id="preview">Content</div>
         </div>
       </div>
     </div>
