9f1ce61e1df3d8726ae4c1815d5913f76661a610
spacescript <simonbengoehring@gmail.com>
2022-07-21 17:11:12 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.scss';
+import React, {useState} from 'react';
 
 function App() {
+  const [markdown, setMarkdown] = useState('');
 
+  // const markdownUpdate = () =>{
+  //   setMarkdown()
+  // }
+
+  function handleChange(event) {
+    setMarkdown(event.target.value)
+  }
 
 
   return (
@@ -9,11 +18,11 @@ function App() {
       <div className="container" id="container">
         <div className="editor-box" id="editor-box">
           <h3>Editor</h3>
-          <textarea name="editor" id="editor" cols="30" rows="10" placeholder="Content"></textarea>
+          <textarea name="editor" id="editor" cols="30" rows="10" placeholder="Content" onChange={handleChange}></textarea>
         </div>
         <div className="preview-box" id="preview-box">
           <h3>Preview</h3>
-          <div className="preview" id="preview">Content</div>
+          <div className="preview" id="preview">{markdown}</div>
         </div>
       </div>
     </div>
