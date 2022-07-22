ae2cbb97f00f2c43647849f07c2f5ed589dc2752
spacescript <simonbengoehring@gmail.com>
2022-07-21 21:03:51 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import React, {useState} from 'react';
 function App() {
   const [markdown, setMarkdown] = useState('');
 
-  // const markdownUpdate = () =>{
-  //   setMarkdown()
-  // }
-
   function handleChange(event) {
     setMarkdown(event.target.value)
   }
@@ -15,16 +11,21 @@ function App() {
 
   return (
     <div className="App">
+
       <div className="container" id="container">
+        
         <div className="editor-box" id="editor-box">
           <h3>Editor</h3>
           <textarea name="editor" id="editor" cols="30" rows="10" placeholder="Content" onChange={handleChange}></textarea>
         </div>
+
         <div className="preview-box" id="preview-box">
           <h3>Preview</h3>
           <div className="preview" id="preview">{markdown}</div>
         </div>
+
       </div>
+
     </div>
   );
 }
