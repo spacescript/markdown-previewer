a0a7b3efb0183db900a4f603e03b1269ec4b81b9
spacescript <simonbengoehring@gmail.com>
2022-07-29 21:23:30 -0700

    Bookmarked Package

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import './App.scss';
-import React, {useState} from 'react';
-// import { marked } from '../js/marked';
-import marked from 'marked';
+// import './App.scss';
+// import React, {useState} from 'react';
+// // import { marked } from '../js/marked';
+// import marked from 'marked';
 
-// marked.setOptions({
-//   breaks: true
-// })
+// // marked.setOptions({
+// //   breaks: true
+// // })
 
-// const renderer = new marked.Renderer();
+// // const renderer = new marked.Renderer();
 
-function App() {
-  const [markdown, setMarkdown] = useState('');
+// function App() {
+//   const [markdown, setMarkdown] = useState('');
 
-  function handleChange(event) {
-    setMarkdown(event.target.value)
-  }
+//   // function handleChange(event) {
+//   //   setMarkdown(event.target.value)
+//   // }
+
+//   // Belongs in editor onChange={handleChange}
 
 
-  return (
-    <div className="App">
+//   return (
+//     <div className="App">
 
-      <div className="container" id="container">
+//       <div className="container" id="container">
         
-        <div className="editor-box" id="editor-box">
-          <h3>Editor</h3>
-          <textarea name="editor" id="editor" cols="30" rows="10" placeholder="Content" onChange={handleChange}></textarea>
-        </div>
+//         <div className="editor-box" id="editor-box">
+//           <h3>Editor</h3>
+//           <textarea name="editor" id="editor" cols="30" rows="10" placeholder="Content" ></textarea>
+//         </div>
 
-        <div className="preview-box" id="preview-box">
-          <h3>Preview</h3>
-          <div className="preview" id="preview">{markdown}</div>
-        </div>
+//         <div className="preview-box" id="preview-box">
+//           <h3>Preview</h3>
+//           <div className="preview" id="preview">{markdown}</div>
+//         </div>
 
-      </div>
+//       </div>
 
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-export default App;
+// // export default App;
